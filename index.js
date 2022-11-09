793e0329157e1b0788bb4e90c12f83ba81e95e02
Md Galib Hossain <mdgalib23@gmail.com>
2022-11-10 03:09:50 +0600

    review deleeted atlas making problem

--- index.js
+++ index.js
@@ -4,32 +4,81 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const services = require("./services.json");
+// const services = require("./services.json");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// getting data
-app.get("/services", (req, res) => {
-  res.send(services);
-});
+// getting data from json
+// app.get("/services", (req, res) => {
+//   res.send(services);
+// });
 
 //
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.app9bcf.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  "mongodb+srv://DB_USER:DB_PASSWORD@cluster0.fkxltzv.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("photography-service").collection("services");
-  // perform actions on the collection object
 
-  client.close();
-});
+//
+//
+async function run() {
+  try {
+    const servicesCollection = client
+      .db("photography-service")
+      .collection("services");
+    const reviewsCollection = client
+      .db("photography-service")
+      .collection("reviews");
+    // load limited services
 
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    // load all services
+    app.get("/allservices", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    // single services
+    app.get("/services/:id", async (req, res) => {
+      const id = parseInt(req.params.id);
+
+      const cursor = servicesCollection.findOne({ serviceid: id });
+
+      const service = await cursor;
+
+      res.send(service);
+    });
+    // load all reviews
+    app.get("/reviews/:id", async (req, res) => {
+      const id = parseInt(req.params.id);
+
+      const cursor = reviewsCollection.find({ serviceid: id });
+      const reviews = await cursor.toArray();
+      console.log;
+      res.send(reviews);
+    });
+    // add review
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 //
 app.listen(port, () => {
   console.log("photography services running on port:", port);
