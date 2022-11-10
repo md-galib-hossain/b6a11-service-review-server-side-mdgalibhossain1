91d3176500e368d2f7157ca9caba40cde2f0726b
Md Galib Hossain <mdgalib23@gmail.com>
2022-11-10 22:59:25 +0600

    Comment

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Fake data
 // getting data from json
 // app.get("/services", (req, res) => {
 //   res.send(services);
