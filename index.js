8a0a8d99aac474810e04d4ce1bd0f7602739dd37
Md Galib Hossain <mdgalib23@gmail.com>
2022-11-10 17:34:43 +0600

    Delete done

--- index.js
+++ index.js
@@ -120,6 +120,13 @@ async function run() {
       const result = await reviewsCollection.updateOne(filter, updatedreview);
       res.send(result);
     });
+    // deleete review
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
